e5f0ae7773e5e8f572230a99033f925f06d05104
Aaron Franks <aaron.franks@gmail.com>
2012-07-22 12:11:57 -0700

    Use a basic templating system for output.

--- garbagepatch.js
+++ garbagepatch.js
@@ -20,6 +20,17 @@
         return (list.indexOf(item) !== -1);
     }
 
+    // Very basic template system
+    // Adapted from http://mir.aculo.us/2011/03/09/little-helpers-a-tweet-sized-javascript-templating-engine/
+    function _template(str, d){
+        for(var p in d) {
+            // Small addition to make '{foo.length}' work in templates:
+            if (p.length) str = str.replace(new RegExp('{'+p+'.length}', 'g'), d[p].length);
+            str = str.replace(new RegExp('{'+p+'}','g'), d[p]);
+        }
+        return str;
+    }
+
     function testOutput() {
         var allGlobals = Object.keys(window);
         var browserGlobals = getBrowserGlobals();
@@ -31,9 +42,10 @@
             else globals.app.push(name);
         });
 
-        document.write('browser: ', globals.browser);
-        document.write('<br />libvars: ', globals.libs);
-        document.write('<br />appvars: ', globals.app);
+        var template = 'browser ({browser.length}): {browser}<br />' +
+                       'libvars ({libs.length}): {libs}<br />' +
+                       'appvars ({app.length}): {app}';
+        document.write(_template(template, globals));
     }
 
     testOutput();
